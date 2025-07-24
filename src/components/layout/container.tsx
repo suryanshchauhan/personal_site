20d3dfc9a0f72f0065a9ddf41d4247126f34997f
Suryansh Chauhan <chauhansuryansh@proton.me>
2025-07-25 03:12:29 +0530

    Reduce section gaps even more

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -9,14 +9,14 @@ const Container = React.forwardRef<HTMLElement, ContainerProps>(
     return (
       <section
         className={mergeClasses(
-          'w-full bg-gray py-8 md:py-10 2xl:py-12',
+          'w-full bg-gray py-2 md:py-3 2xl:py-4',
           className
         )}
         ref={ref}
         {...props}
       >
         <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 px-4 md:gap-12 md:px-8">
-          {children}
+        <div className="mx-auto flex w-full max-w-7xl flex-col gap-3 px-4 md:gap-6 md:px-8">
         </div>
       </section>
     );
