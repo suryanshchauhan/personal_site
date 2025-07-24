dcfd9dea7196e7de1ef4f7a6e98b5d83365f52d5
Suryansh Chauhan <chauhansuryansh@proton.me>
2025-07-25 03:13:05 +0530

    Fix JSX syntax error in container component

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -16,17 +16,8 @@ const Container = React.forwardRef<HTMLElement, ContainerProps>(
         {...props}
       >
         <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 px-4 md:gap-12 md:px-8">
-        <div className="mx-auto flex w-full max-w-7xl flex-col gap-3 px-4 md:gap-6 md:px-8">
+          {children}
         </div>
       </section>
     );
   }
-);
-
-Container.displayName = 'Container';
-
-export default Container;
-
-    )
-  }
-)
