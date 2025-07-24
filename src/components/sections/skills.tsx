6499a50bab97661c5d150bd8ebd28eb1c8a29771
Suryansh Chauhan <chauhansuryansh@proton.me>
2025-07-25 03:25:25 +0530

    Reduce space between hero and skills by 50%

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -6,7 +6,7 @@ import { TECHNOLOGIES } from '@/lib/data';
 
 const SkillsSection = () => {
   return (
-    <Container id='skills' className='pb-6 md:pb-8 2xl:pb-10'>
+    <Container id='skills' className='py-6 md:py-8 2xl:py-10 pb-6 md:pb-8 2xl:pb-10'>
       <div className="flex flex-col items-center gap-4">
         <div className="self-center">
           <Tag label="Skills" />
