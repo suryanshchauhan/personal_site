7b40d077270d0bba91407c4dcb29178bec762c2d
Suryansh Chauhan <chauhansuryansh@proton.me>
2025-07-25 03:27:40 +0530

    Updated skills.tsx

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -6,7 +6,7 @@ import { TECHNOLOGIES } from '@/lib/data';
 
 const SkillsSection = () => {
   return (
-    <Container id='skills' className='pt-2 md:pt-3 2xl:pt-4 pb-6 md:pb-8 2xl:pb-10'>
+    <Container id='skills' className='pt-1 md:pt-1 2xl:pt-1 pb-16 md:pb-20 2xl:pb-24'>
       <div className="flex flex-col items-center gap-4">
         <div className="self-center">
           <Tag label="Skills" />
