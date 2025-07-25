ca21b230622581d80f4defe6a527d67c03cf45e1
Suryansh Chauhan <chauhansuryansh@proton.me>
2025-07-26 03:40:59 +0530

    Display all experiences simultaneously without clicking

--- src/components/sections/experiences.tsx
+++ src/components/sections/experiences.tsx
@@ -1,15 +1,12 @@
 'use client';
 
-import { useState } from 'react';
 import { EXPERIENCES } from '@/lib/data';
 import ExperienceDetails from '@/components/data-display/experience-details';
 import Tag from '@/components/data-display/tag';
 import Typography from '@/components/general/typography';
 import Container from '@/components/layout/container';
-import ImageWrapper from '@/components/data-display/image-wrapper';
 
 const ExperienceSection = () => {
-  const [selectedExperience, setSelectedExperience] = useState(0);
 
   return (
     <Container id='experience' className='bg-gray-50'>
@@ -25,37 +22,14 @@ const ExperienceSection = () => {
         </Typography>
       </div>
 
-      {/* Experience layout with logos on the right */}
-      <div className='flex flex-col lg:flex-row gap-8 max-w-6xl mx-auto'>
-        {/* Company logos column - moved to left */}
-        <div className='flex flex-row lg:flex-col items-center justify-center gap-4 lg:max-w-xs order-2 lg:order-1'>
-          {EXPERIENCES?.map((experience, index) => (
-            <button
-              key={index}
-              onClick={() => setSelectedExperience(index)}
-              className={`flex items-center justify-center p-4 rounded-xl transition-all duration-200 hover:bg-white hover:shadow-md w-20 h-20 lg:w-full lg:h-20 ${
-                selectedExperience === index 
-                  ? 'bg-white shadow-lg ring-2 ring-gray-900/20 dark:bg-black dark:ring-black/20' 
-                  : 'bg-gray-100 hover:bg-white hover:shadow-lg dark:hover:bg-black'
-              }`}
-            >
-              <ImageWrapper
-                src={experience.logo}
-                srcForDarkMode={experience.darkModeLogo}
-                alt={experience.logoAlt}
-                className='max-w-[60px] max-h-[40px] lg:max-w-[100px] lg:max-h-[50px] object-contain transition-transform duration-200 hover:scale-105'
-              />
-            </button>
-          ))}
-        </div>
-        
-        {/* Selected experience details */}
-        <div className='flex-1 order-1 lg:order-2'>
+      {/* All experiences displayed in a grid */}
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 max-w-6xl mx-auto'>
+        {EXPERIENCES?.map((experience, index) => (
           <ExperienceDetails
-            {...EXPERIENCES[selectedExperience]}
-            key={selectedExperience}
+            key={index}
+            {...experience}
           />
-        </div>
+        ))}
       </div>
     </Container>
   );
