609c896ac4532261d06d1a817f5c38176f8b9041
Suryansh Chauhan <chauhansuryansh@proton.me>
2025-07-26 04:13:11 +0530

    Updated typography.tsx

--- src/components/general/typography.tsx
+++ src/components/general/typography.tsx
@@ -15,7 +15,6 @@ const translations: { [key: string]: string } = {
   "Making pretty software ✨ using cutting-edge fullstack and front-end technologies for agentic AI platforms.": "Erstelle schöne Software ✨ mit modernsten Fullstack- und Frontend-Technologien für agentische KI-Plattformen.",
   "United States of America": "Vereinigte Staaten von Amerika",
   "indianapolis": "indianapolis",
-  "Location": "Standort",
   
   // Skills Section
   "Skills": "Fähigkeiten",
