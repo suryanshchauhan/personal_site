e1bb2a95d2bb07efb0b38f454e915b0dacb9add9
Suryansh Chauhan <chauhansuryansh@proton.me>
2025-07-26 18:16:23 +0530

    Move American flag to end of location text

--- src/components/data-display/education-details.tsx
+++ src/components/data-display/education-details.tsx
@@ -64,7 +64,7 @@ const EducationDetails = ({
         <div className="flex flex-col gap-3 flex-grow">
           <div className="flex flex-col gap-2">
             <Typography variant="body2" className="text-gray-600">
-              🇺🇸 {location}
+              {location} 🇺🇸
             </Typography>
             <Typography variant="h3" className="font-semibold text-gray-900">
               {school}
